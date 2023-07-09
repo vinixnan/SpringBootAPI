25f8ca2adc9f4e3f738dca8a845bde66244180ec
Vinicius Renan <vinyciux@gmail.com>
2023-07-09 00:47:53 -0300

    Update to run

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ ARG MONGO_DB_PASS
 RUN mkdir -p /code
 WORKDIR /code
 COPY . /code/
+RUN cp /code/src/main/resources/application.properties /code/src/main/resources/application.properties_org
 RUN echo "spring.data.mongodb.username="$MONGO_DB_USERNAME >> /code/src/main/resources/application.properties
 RUN echo "spring.data.mongodb.password="$MONGO_DB_PASS >> /code/src/main/resources/application.properties
 RUN cd /code/ && mvn clean install
-EXPOSE 8102
+EXPOSE 80
---
+++ update_mongo_vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MONGO_URL="mongodb://$MONGO_USERNAME:$MONGO_PASSWORD@mongodb:27017/"
+
+cp /code/src/main/resources/application.properties_org /code/src/main/resources/application.properties
+echo "spring.data.mongodb.username="$MONGO_INITDB_ROOT_USERNAME >> /code/src/main/resources/application.properties
+echo "spring.data.mongodb.password="$MONGO_INITDB_ROOT_PASSWORD >> /code/src/main/resources/application.properties
+cd /code/ && mvn spring-boot:run
